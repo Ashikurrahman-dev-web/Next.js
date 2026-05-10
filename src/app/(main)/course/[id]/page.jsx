476a4a7c8ea3eba0303fe6146f126d12b066d424
ashikurrahman88722-code <ashikurrahman88722@gmail.com>
2026-05-10 18:11:27 +0600

    fix

--- src/app/(main)/course/[id]/page.jsx
+++ src/app/(main)/course/[id]/page.jsx
@@ -1,58 +1,57 @@
-import Link from "next/link";
+import Link from "next/link"
 import React from "react";
-import EnrollModal from "@/component/shared/EnrollModal";
-import courses from "@/data/course.json";
-
-async function getCourse(id) {
-  return courses.find((course) => course.id === Number(id));
+import EnrollModal from "@/component/shared/EnrollModal"
+async function getCourse(id) { const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/course.json`, {
+  cache: "no-store",
+});
+const data = await res.json();
+return data.find((course) => course.id == id);
 }
 
 const CourseDetailsPage = async ({ params }) => {
-  const { id } = params;
+   const { id } = await params;
+ const course = await getCourse(id);
 
-  const course = await getCourse(id);
 
-  if (!course) {
-    return <h1>Course not found</h1>;
-  }
+ return (
+ <div className="max-w-5xl mx-auto p-6 mt-10">
+ <div className="bg-white shadow-lg rounded-2xl overflow-hidden">
 
-  return (
-    <div className="max-w-5xl mx-auto p-6 mt-10">
-      <div className="bg-white shadow-lg rounded-2xl overflow-hidden">
-        <img
-          src={course.image}
-          alt={course.title}
-          className="w-full h-150 object-cover"
-        />
+<img
+ src={course.image}
+ alt={course.title}
+ className="w-full h-150 object-cover"
+ />
 
-        <div className="p-6">
-          <h1 className="text-3xl font-bold mb-4">
-            {course.title}
-          </h1>
 
-          <p className="text-gray-600 mb-6">
-            {course.description}
-          </p>
+ <div className="p-6">
+ <h1 className="text-3xl font-bold mb-4">
+ {course.title}
+ </h1>
 
-          <div className="grid grid-cols-2 gap-4 text-sm mb-6">
-            <p><strong>Instructor:</strong> {course.instructor}</p>
-            <p><strong>Duration:</strong> {course.duration}</p>
-            <p><strong>Rating:</strong> ⭐ {course.rating}</p>
-            <p><strong>Category:</strong> {course.category}</p>
-            <p><strong>Level:</strong> {course.level}</p>
-          </div>
+ <p className="text-gray-600 mb-6">
+ {course.description}
+ </p>
 
-          <div className="flex items-center justify-between">
-            <EnrollModal />
 
-            <button className="bg-[#0f0524] cursor-pointer text-white px-6 py-3 rounded-xl">
-              <Link href="/allcourse">Go back</Link>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+<div className="grid grid-cols-2 gap-4 text-sm mb-6">
+ <p><strong>Instructor:</strong> {course.instructor}</p>
+ <p><strong>Duration:</strong> {course.duration}</p>
+ <p><strong>Rating:</strong> ⭐ {course.rating}</p>
+ <p><strong>Category:</strong> {course.category}</p>
+ <p><strong>Level:</strong> {course.level}</p>
+ </div>
+
+
+ <div className="flex items-center justify-between"><EnrollModal />
+<button className="bg-[#0f0524] cursor-pointer text-white px-6 py-3 rounded-xl">
+<Link href={"/allcourse"}>Go back</Link> 
+ </button>
+ </div>
+ </div>
+ </div>
+</div>
+ );
 };
 
 export default CourseDetailsPage;
