6f29dc5a9924cd0986ad2dff945f2f90d6973742
ashikurrahman88722-code <ashikurrahman88722@gmail.com>
2026-05-10 17:33:49 +0600

    Fix in course id page

--- src/app/(main)/course/[id]/page.jsx
+++ src/app/(main)/course/[id]/page.jsx
@@ -1,29 +1,26 @@
-import Link from "next/link"
+import Link from "next/link";
 import React from "react";
-import EnrollModal from "@/component/shared/EnrollModal"
+import EnrollModal from "@/component/shared/EnrollModal";
+import courses from "@/../public/course.json";
+
 async function getCourse(id) {
-  const res = await fetch("http://localhost:3000/course.json");
-  const data = await res.json();
-  return data.find((course) => course.id == id);
+  return courses.find((course) => course.id == id);
 }
 
 const CourseDetailsPage = async ({ params }) => {
-    const { id } = await params;
-  const course = await getCourse(id);
+  const { id } = params;
 
-  
+  const course = await getCourse(id);
 
   return (
     <div className="max-w-5xl mx-auto p-6 mt-10">
       <div className="bg-white shadow-lg rounded-2xl overflow-hidden">
-    
-       <img
+        <img
           src={course.image}
           alt={course.title}
           className="w-full h-150 object-cover"
         />
 
-        
         <div className="p-6">
           <h1 className="text-3xl font-bold mb-4">
             {course.title}
@@ -33,7 +30,6 @@ const CourseDetailsPage = async ({ params }) => {
             {course.description}
           </p>
 
-          
           <div className="grid grid-cols-2 gap-4 text-sm mb-6">
             <p><strong>Instructor:</strong> {course.instructor}</p>
             <p><strong>Duration:</strong> {course.duration}</p>
@@ -42,11 +38,12 @@ const CourseDetailsPage = async ({ params }) => {
             <p><strong>Level:</strong> {course.level}</p>
           </div>
 
-          
-          <div className="flex items-center justify-between"><EnrollModal />
-          <button className="bg-[#0f0524] cursor-pointer text-white px-6 py-3 rounded-xl">
-           <Link href={"/allcourse"}>Go back</Link> 
-          </button>
+          <div className="flex items-center justify-between">
+            <EnrollModal />
+
+            <button className="bg-[#0f0524] cursor-pointer text-white px-6 py-3 rounded-xl">
+              <Link href={"/allcourse"}>Go back</Link>
+            </button>
           </div>
         </div>
       </div>
