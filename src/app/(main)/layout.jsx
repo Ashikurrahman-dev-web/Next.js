973ba445ddc078dd2f6b7743593de7b0ea244071
ashikurrahman88722-code <ashikurrahman88722@gmail.com>
2026-05-19 21:19:49 +0600

    layout

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Navbar from "@/component/shared/Navbar";
 import Footer from "@/component/shared/Footer";
-import { UserProvider } from "@/context/UserContext";
+
 const MainLayout = ({ children }) => {
     return (
         <>
          
-          <UserProvider>
+          
             <Navbar />
             {children}
             <Footer /> 
- </UserProvider>
+
            
         </>
     );
