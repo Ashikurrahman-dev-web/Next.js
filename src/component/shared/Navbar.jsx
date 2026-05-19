488e3c7a41756b1bf38d51e38993aa552bd22d6c
ashikurrahman88722-code <ashikurrahman88722@gmail.com>
2026-05-19 20:50:19 +0600

    avater added

--- src/component/shared/Navbar.jsx
+++ src/component/shared/Navbar.jsx
@@ -2,45 +2,36 @@
 import React, { useState } from 'react';
 import { Menu, X } from 'lucide-react';
 import NavLink from "@/component/shared/NavLink";
-import avatar from "@/image/avatar.jpg"
-import Image from 'next/image';
-import { useUser } from "@/context/UserContext";
+import { authClient } from '@/lib/auth-client';
+import { Avatar } from "@heroui/react";
 const Navbar = () => {
+  const userData = authClient.useSession();
+  const user = userData?.user;
   const [menuOpen, setMenuOpen] = useState(false);
-  const { user } = useUser();
   return (
     <nav className="px-6 md:px-8 py-4 bg-white border-b border-gray-100">
       
       <div className="flex items-center justify-between">
-        
-        
         <h1 className="text-2xl font-extrabold tracking-tight">
           <span className="text-slate-900">Course</span>
           <span className="text-emerald-800">Hub</span>
         </h1>
         <div className="hidden md:flex items-center space-x-8 text-gray-600 font-medium">
          <NavLink href={"/home"}> <button className="cursor-pointer">Home</button></NavLink>
-        <NavLink href={"/allcourse"}>  <button className="cursor-pointer">All Course</button></NavLink>
+    <NavLink href={"/allcourse"}>  <button className="cursor-pointer">All Course</button></NavLink>
         <NavLink href={"/myprofile"}>  <button className="cursor-pointer">My Profile</button></NavLink>
         </div>
         <div className="flex items-center space-x-4 md:space-x-6">
-          <div className="hidden md:flex items-center gap-3">
-  <Image
-    src={user?.image || avatar}
-    alt="avatar"
-    width={40}
-    height={40}
-    className="rounded-full"
-  />
-  
-  <div className="text-sm font-medium text-gray-600">
-    {user?.name || "Name"}
-  </div>
-</div>
-          
-          
-          
-      <NavLink   href={"/"}>    <button
+          <div className="md:flex items-center gap-3">
+  {user && (
+            <div className="flex gap-4 items-center">
+              <Avatar size="sm">
+                <Avatar.Image alt={user.name} src={user?.image} referrerPolicy="no-referrer" />
+                <Avatar.Fallback>{user.name[0]}</Avatar.Fallback>
+              </Avatar>
+            </div>
+          )}
+  <NavLink   href={"/"}>    <button
 className="cursor-pointer hidden md:block bg-[#7C3AED] text-white px-6 py-2 rounded-full font-medium hover:bg-[#6D28D9] transition shadow-sm">
              
           Log Out
@@ -54,6 +45,7 @@ className="cursor-pointer hidden md:block bg-[#7C3AED] text-white px-6 py-2 roun
           </button>
         </div>
       </div>
+    </div>
       {menuOpen && (
         <div className="mt-4 flex flex-col space-y-4 md:hidden text-gray-600 font-medium">
          <NavLink href={"/home"}> <button className="cursor-pointer">Home</button></NavLink>
@@ -61,21 +53,9 @@ className="cursor-pointer hidden md:block bg-[#7C3AED] text-white px-6 py-2 roun
         <NavLink href={"/myprofile"}>  <button className="cursor-pointer">My Profile</button></NavLink>
 
           <div className="border-t pt-4 flex flex-col space-y-3">
-            <div className="flex items-center gap-3">
-  <Image
-    src={user?.image || avatar}
-    alt="avatar"
-    width={40}
-    height={40}
-    className="rounded-full"
-  />
-  
-  <div className="text-sm font-medium text-gray-600">
-    {user?.name || "Name"}
-  </div>
-</div>
+            
            <NavLink
-               href={"/"}> <button className="bg-[#7C3AED] text-white py-2 rounded-full coursor-pointer">
+               href={"/"}> <button className="bg-[#7C3AED] text-white py-2 rounded-full cursor-pointer">
               
                 Log Out</button></NavLink>
             </div>
